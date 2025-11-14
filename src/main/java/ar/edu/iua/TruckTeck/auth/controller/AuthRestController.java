30d5e0c3b2f90bbe7d51ce1ea7e54d8f57365ab5
Gastón Zaragosi <gzaragosi@gmail.com>
2025-11-14 17:48:07 -0300

    Agrego lógica y archivos iniciales de login con JWT

---
+++ src/main/java/ar/edu/iua/TruckTeck/auth/controller/AuthRestController.java
@@ -0,0 +1,5 @@
+package ar.edu.iua.TruckTeck.auth.controller;
+
+public class AuthRestController {
+
+}
---
+++ src/main/java/ar/edu/iua/TruckTeck/auth/custom/CustomAuthenticationManager.java
@@ -0,0 +1,5 @@
+package ar.edu.iua.TruckTeck.auth.custom;
+
+public class CustomAuthenticationManager {
+
+}
